6f92ce76130751184e952ce833b7d1c4d3240b88
klauslawson <klauslawsondjecky56@gmail.com>
2022-03-22 22:59:29 -0500

    new file

---
+++ SortWebTables.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumStudy
+{
+    class SortWebTables
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+            driver = new EdgeDriver();
+            // implicit wait 5sec can be declare globaly
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/#/offer";
+        }
+        [Test]
+        public void SortTables()
+        {
+
+        }
+    }
+}
